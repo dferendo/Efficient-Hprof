69e4b7f2c2e1b797380355c4c3c09ba87b727322
ferendo <dylanfrendo@gmail.com>
2017-09-18 14:16:41 +0200

    Updated benchmarks

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+# HProf Modification
+
+This project is done to check if some improvements can be made to hprof. Add dependencies were added to the repository and Java version 8 is used. This version only compiles on linux, solaris and bsd, for other versions modify the lib folder to your system required files (These can be found when downloading jdk8 source code). 
+
+## Benchmarks
+
+To check the verify the improvements made, DaCapo benchmarks were used. [Link](https://sourceforge.net/projects/dacapobench/files/)
+
 ## Compilation
 
-To compile hprof, use cmake. Change JAVA_HOME as required to your environment. The shared library will be created in cmake-build-debug with the name "libhprof.so"
+To compile hprof, use cmake. The shared library will be created in cmake-build-debug with the name "libhprof.so"
 
 
--- run-bench-marks.sh
+++ run-bench-marks.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd ./cmake-build-debug
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar avrora
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar batik
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar eclipse
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar fop
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar h2
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar jython
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar luindex
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar lusearch
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar pmd
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar sunflow
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar tomcat
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar tradebeans
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar tradesoap
-java -agentlib:hprof -jar ../benchmarks/dacapo-9.12-bach.jar xalan
+cd ./benchmarks
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar dacapo-9.12-bach.jar avrora
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar batik
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar eclipse
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar fop
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar h2
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar jython
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar luindex
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar lusearch
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar pmd
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar sunflow
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar tomcat
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar tradebeans
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar tradesoap
+java -agentpath:"../cmake-build-debug/libhprof.so" -jar ../benchmarks/dacapo-9.12-bach.jar xalan
 
 
