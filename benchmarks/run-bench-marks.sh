cffee2cc1fb8696b851e1c2cb2b67706d20c6fe7
ferendo <dylanfrendo@gmail.com>
2018-03-12 21:07:45 +0100

    Added libraries for testing, spreadsheet template and updated script

--- benchmarks/run-bench-marks.sh
+++ benchmarks/run-bench-marks.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Array of shared libraries
-declare -a libraries=("Nohprof" "hprof")
+declare -a libraries=("Nohprof" "libadding_call_return_bci_calls_calls_do_nothing.so"
+					  "libbci_events_only.so" "libbci_no_call_event.so"
+					  "libbci_no_new_array_event.so" "libbci_no_object_event.so"
+					  "libbci_no_return_event.so" "libhprof.so"
+					  "libjvmti_events_only.so" "libremove_any_allocation_method.so"
+					  "libremove_get_trace_and_any_allocation_method.so")
 
 # Array of programs to test using dacapo benchmarks
 declare -a programs=("avrora" "batik" "fop" "h2" "jython" "luindex" "lusearch" "pmd" "sunflow" "tradebeans" "tradesoap" "xalan")
@@ -18,13 +23,10 @@ do
 		do
 
 			if [ "$library" == "${libraries[0]}" ]; then
-			   java -jar dacapo-9.12-bach.jar "$program" -s "$size"
-			elif [ "$library" == "${libraries[0]}" ]; then
-			   java -jar dacapo-9.12-bach.jar "$program" -s "$size"
+			   echo java -jar dacapo-9.12-bach.jar "$program" -s "$size"
 			else
-			   
+			   echo java -agentpath:"\"./hprof_libraries/$library=heap=dump\"" -jar dacapo-9.12-bach.jar "$program" -s "$size"
 			fi
-
 		done
 	done
 done
