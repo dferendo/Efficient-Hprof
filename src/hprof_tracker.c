faf9f54d8d7feafcd693c953b1f5431b9d8ea0be
ferendo <dylanfrendo@gmail.com>
2018-03-12 19:45:52 +0100

    Removed return event

--- src/hprof_tracker.c
+++ src/hprof_tracker.c
@@ -159,9 +159,9 @@ static void JNICALL
 Tracker_nativeReturnSite
   (JNIEnv *env, jclass clazz, jobject thread, jint cnum, jint mnum)
 {
-    BEGIN_TRACKER_CALLBACK() {
-        event_return(env, thread, cnum, mnum);
-    } END_TRACKER_CALLBACK();
+//    BEGIN_TRACKER_CALLBACK() {
+//        event_return(env, thread, cnum, mnum);
+//    } END_TRACKER_CALLBACK();
 }
 
 
