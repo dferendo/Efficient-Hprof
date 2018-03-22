595604194f002d64cdb0f5f841a2604e360e6da6
Dylan <dylanfrendo@gmail.com>
2018-03-22 13:21:23 +0100

    Added test cases

---
+++ benchmarks/Class Tests/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String args[]) {
+		Test test = new Test();
+
+		test.method1();
+	}
+
+	public void method1() {
+		int variable1 = 8; 
+
+		method2();
+	}
+
+	public void method2() {
+		long variable2 = 10;
+
+		return;
+	}
+}
